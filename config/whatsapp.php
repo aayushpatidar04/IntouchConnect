173f8be6eec264735662ead81c5f86d04ac2f337
Aayush Patidar <aayushpatidar04@gmail.com>
2026-04-25 09:38:00 +0530

    whatsapp gateway live

--- config/whatsapp.php
+++ config/whatsapp.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'gateway_url'    => env('WHATSAPP_GATEWAY_URL', 'https://mustang-cuddly-terminally.ngrok-free.app'),
+    'gateway_url'    => env('WHATSAPP_GATEWAY_URL', 'https://whatsapp.intouchsoftwaresolution.in'),
     'gateway_secret' => env('WHATSAPP_GATEWAY_SECRET', 'aayush-patidar'),
 ];
