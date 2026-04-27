487b3a652a6505cdfb84baf24a508d2f5e8f4521
Aayush Patidar <aayushpatidar04@gmail.com>
2026-04-27 12:10:49 +0530

    ngrok back

--- config/whatsapp.php
+++ config/whatsapp.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'gateway_url'    => env('WHATSAPP_GATEWAY_URL', 'https://whatsapp.intouchsoftwaresolution.in'),
-    // 'gateway_url'    => env('WHATSAPP_GATEWAY_URL', 'https://mustang-cuddly-terminally.ngrok-free.app'),
+    // 'gateway_url'    => env('WHATSAPP_GATEWAY_URL', 'https://whatsapp.intouchsoftwaresolution.in'),
+    'gateway_url'    => env('WHATSAPP_GATEWAY_URL', 'https://mustang-cuddly-terminally.ngrok-free.app'),
     // 'gateway_url'    => env('WHATSAPP_GATEWAY_URL', 'http://127.0.0.1:3000'),
     'gateway_secret' => env('WHATSAPP_GATEWAY_SECRET', 'aayush-patidar'),
 ];
